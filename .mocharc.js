10231e4a9874a1e20fe03b27c533e029f8eceabd
A. D. Prasad <adprasad.89@gmail.com>
2020-10-03 12:55:39 -0700

    test(init): adding initial test configuration

---
+++ .mocharc.js
@@ -0,0 +1,25 @@
+let timeout = 5000;
+if (process.env.MOCHA_TIMEOUT) {
+  timeout = MOCHA_TIMEOUT;
+}
+
+const config = {
+  "bail": true,
+  "enable-source-maps": true,
+  "extensions": ["ts"],
+  "recursive": true,
+  "require": ["ts-node/register"],
+  "throw-deprecation": true,
+  "timeout": timeout,
+}
+
+if (process.env.MOCHA_THROW_DEPRECATION === 'false') {
+  delete config['throw-deprecation'];
+}
+if (process.env.MOCHA_REPORTER) {
+  config.reporter = process.env.MOCHA_REPORTER;
+}
+if (process.env.MOCHA_REPORTER_OUTPUT) {
+  config['reporter-option'] = `output=${process.env.MOCHA_REPORTER_OUTPUT}`;
+}
+module.exports = config
