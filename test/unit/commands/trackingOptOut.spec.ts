543eb6630e9bfc0edd76ef0669954a11e9fc9f34
A. D. Prasad <adprasad@users.noreply.github.com>
2020-10-04 15:51:02 -0700

    refactor(opt-out): reorganize subcommand tests

--- test/unit/commands/trackingOptOut.spec.ts
+++ test/unit/commands/trackingOptOut.spec.ts
@@ -8,10 +8,10 @@ import { ImportMock, MockManager } from "ts-mock-imports";
 const sandbox = createSandbox();
 
 import {
-  deleteCallback,
+  removeCallback,
   MESSAGE_COMMAND_INVALID,
   MESSAGE_SUBCOMMAND_INVALID,
-  saveCallBack,
+  addCallBack,
   trackingOptOut,
 } from "../../../src/commands/trackingOptOut";
 
@@ -79,107 +79,107 @@ describe("command opt-out", () => {
     });
   });
 
-  context("when subcommand add", () => {
-    context("database available", () => {
-      describe("command: !optOut add", () => {
-        describe("user not in database", () => {
-          it("attempt to add user id to database", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "   |optOut                  add   ",
-              "123456789",
-              "author"
-            );
-            const document: TOO.TrackingOptOutInt = {
-              userId: "123456789",
-            } as TOO.TrackingOptOutInt;
+  context("when subcommand valid", () => {
+    describe("command: !optOut add", () => {
+      it("attempt to add user id to database", async () => {
+        const testMessage: Message = buildMessageWithContent(
+          "   |optOut                  add   ",
+          "123456789",
+          "author"
+        );
+        const document: TOO.TrackingOptOutInt = {
+          userId: "123456789",
+        } as TOO.TrackingOptOutInt;
 
-            await trackingOptOut.command(testMessage);
+        await trackingOptOut.command(testMessage);
 
-            expect(TrackingOptOutMock).calledOnceWith({
-              userId: "123456789",
-            });
-          });
-          it("should notify user they are now opt-out", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "|optOut add",
-              "123456789",
-              "author"
-            );
-            const document: TOO.TrackingOptOutInt = {
-              userId: "123456789",
-            } as TOO.TrackingOptOutInt;
-            await saveCallBack(testMessage, "author")(null, document);
-
-            expect(testMessage.channel.send).calledWith(
-              `@author, you are now opt-out of tracking.`
-            );
-          });
-          it("should notify user if opt-out failed", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "|optOut add",
-              "123456789",
-              "author"
-            );
-            const document: TOO.TrackingOptOutInt = {
-              userId: "123456789",
-            } as TOO.TrackingOptOutInt;
-            try {
-              await saveCallBack(testMessage, "author")(
-                new Error("Something"),
-                null
-              );
-            } catch (error) {
-              expect(testMessage.channel.send).calledWith(
-                `Oops, @author, something went wrong. Please try again in a few minutes.`
-              );
-            }
-          });
+        expect(TrackingOptOutMock).calledOnceWith({
+          userId: "123456789",
         });
-        describe("user is in the database", () => {
-          beforeEach(() => {
-            ImportMock.restore();
-            sandbox.stub(TOO.TrackingOptOut, "find");
-            sandbox.stub(TOO.TrackingOptOut, "deleteMany");
-          });
-          it("call deleteMany to ensure record not in db", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "   |optOut remove   ",
-              "123456789",
-              "author"
+      });
+      describe("addCallBack called", () => {
+        it("should notify user they are now opt-out", async () => {
+          const testMessage: Message = buildMessageWithContent(
+            "|optOut add",
+            "123456789",
+            "author"
+          );
+          const document: TOO.TrackingOptOutInt = {
+            userId: "123456789",
+          } as TOO.TrackingOptOutInt;
+          await addCallBack(testMessage, "author")(null, document);
+
+          expect(testMessage.channel.send).calledWith(
+            `@author, you are now opt-out of tracking.`
+          );
+        });
+        it("should notify user if opt-out failed", async () => {
+          const testMessage: Message = buildMessageWithContent(
+            "|optOut add",
+            "123456789",
+            "author"
+          );
+          const document: TOO.TrackingOptOutInt = {
+            userId: "123456789",
+          } as TOO.TrackingOptOutInt;
+          try {
+            await addCallBack(testMessage, "author")(
+              new Error("Something"),
+              null
             );
-
-            await trackingOptOut.command(testMessage);
-
-            expect(TOO.TrackingOptOut.deleteMany).calledWith({
-              userId: "123456789",
-            });
-          });
-          it("warn user if error occured", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "   |optOut remove   ",
-              "123456789",
-              "author"
-            );
-
-            await deleteCallback(testMessage)(new Error());
-
+          } catch (error) {
             expect(testMessage.channel.send).calledWith(
               `Oops, @author, something went wrong. Please try again in a few minutes.`
             );
-          });
-          it("notify user of status change", async () => {
-            const testMessage: Message = buildMessageWithContent(
-              "   |optOut remove   ",
-              "123456789",
-              "author"
-            );
+          }
+        });
+      });
+    });
+    describe("command: !optOut remove", () => {
+      beforeEach(() => {
+        ImportMock.restore();
+        sandbox.stub(TOO.TrackingOptOut, "find");
+        sandbox.stub(TOO.TrackingOptOut, "deleteMany");
+      });
+      it("call deleteMany to remove records", async () => {
+        const testMessage: Message = buildMessageWithContent(
+          "   |optOut remove   ",
+          "123456789",
+          "author"
+        );
 
-            await deleteCallback(testMessage)(null);
+        await trackingOptOut.command(testMessage);
 
-            expect(testMessage.channel.send).calledWith(
-              `@author, you are now opted into tracking.`
-            );
-          });
+        expect(TOO.TrackingOptOut.deleteMany).calledWith({
+          userId: "123456789",
+        });
+      });
+      describe("removeCallBack called", () => {
+        it("warn user if error occured", async () => {
+          const testMessage: Message = buildMessageWithContent(
+            "   |optOut remove   ",
+            "123456789",
+            "author"
+          );
+
+          await removeCallback(testMessage)(new Error());
+
+          expect(testMessage.channel.send).calledWith(
+            `Oops, @author, something went wrong. Please try again in a few minutes.`
+          );
+        });
+        it("notify user of status change", async () => {
+          const testMessage: Message = buildMessageWithContent(
+            "   |optOut remove   ",
+            "123456789",
+            "author"
+          );
+
+          await removeCallback(testMessage)(null);
+
+          expect(testMessage.channel.send).calledWith(
+            `@author, you are now opted into tracking.`
+          );
         });
       });
     });
