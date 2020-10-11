c00d5e34c0788c9957c8fb61638c94432c137731
A. D. Prasad <adprasad@users.noreply.github.com>
2020-10-11 13:21:59 -0700

    test(event): add tests for onMessage Covers: * direct messages * non-discord server * wrong prefix * listener call

--- test/unit/events/onMessage.spec.ts
+++ test/unit/events/onMessage.spec.ts
@@ -1,16 +1,16 @@
 import { SinonSandbox, createSandbox, SinonStub, clock } from "sinon";
-import { mock } from "ts-mockito";
+import { mock, reset } from "ts-mockito";
 import CommandInt from "@Interfaces/CommandInt";
 import ListenerInt from "@Interfaces/ListenerInt";
 import onMessage from "@Events/onMessage";
 import extendsClientToClientInt from "@Utils/extendsClientToClientInt";
 import { getListeners } from "@Utils/readDirectory";
-import * as IUL from "@Listeners/interceptableUsageListener";
 import { expect } from "chai";
 import * as discordjs from "discord.js";
 
 describe("onMessage event", () => {
   const testPrefix = "☂";
+  const MAGIC_TIMEOUT = 3000;
   let sandbox: SinonSandbox;
   const mockCmd = (name = "mockCmd", run: SinonStub): CommandInt => ({
     names: [name, `mock ${name}`],
@@ -31,6 +31,7 @@ describe("onMessage event", () => {
   });
 
   afterEach(() => {
+    reset();
     sandbox.restore();
   });
 
@@ -56,7 +57,7 @@ describe("onMessage event", () => {
       });
 
       const msgPromise = onMessage(msg, clientInt);
-      await sandbox.clock.tickAsync(3000);
+      await sandbox.clock.tickAsync(MAGIC_TIMEOUT);
       await msgPromise;
 
       const inUselistener =
@@ -88,12 +89,109 @@ describe("onMessage event", () => {
       });
 
       const msgPromise = onMessage(msg, clientInt);
-      await sandbox.clock.tickAsync(3000);
+      await sandbox.clock.tickAsync(MAGIC_TIMEOUT);
       await msgPromise;
 
       const inUselistener = clientInt.customListeners["heartsListener"];
 
       expect(inUselistener.run).calledOnce;
+    });
+  });
+
+  context("is direct message", () => {
+    it("should send warning", async () => {
+      const aboutStub = sandbox.stub().resolves();
+      const client = mock<discordjs.Client>();
+      const msg = mock<discordjs.Message>();
+      const author = mock<discordjs.User>();
+      author.id = "1";
+      msg.author = author;
+      msg.content = `${testPrefix}about`;
+      msg.attachments = new discordjs.Collection();
+      msg.channel.startTyping = sandbox.stub();
+      msg.channel.stopTyping = sandbox.stub();
+      msg.channel.send = sandbox.stub().resolves();
+      msg.channel.type = "dm";
+      msg.guild.id = "server_id";
+
+      const clientInt = extendsClientToClientInt(client);
+      clientInt.prefix = { server_id: testPrefix };
+      clientInt.user = mock<discordjs.User>();
+      clientInt.user.id = "2";
+      clientInt.commands = { about: mockCmd("about", aboutStub) };
+      clientInt.customListeners = await getListeners();
+      Object.keys(clientInt.customListeners).forEach((key) => {
+        const stub = sandbox.stub();
+        stub.resolves();
+        clientInt.customListeners[key] = mockListener(key, stub);
+      });
+
+      const msgPromise = onMessage(msg, clientInt);
+      await sandbox.clock.tickAsync(MAGIC_TIMEOUT);
+      await msgPromise;
+
+      expect(msg.channel.startTyping).calledOnce;
+      expect(msg.channel.stopTyping).calledOnce;
+      expect(msg.channel.send).calledOnce;
+      expect(aboutStub).not.called;
+    });
+  });
+  context("is not a discord server", () => {
+    it("should return without calling command", async () => {
+      const aboutStub = sandbox.stub().resolves();
+      const client = mock<discordjs.Client>();
+      const msg = mock<discordjs.Message>();
+      const author = mock<discordjs.User>();
+      author.id = "1";
+      msg.author = author;
+      msg.content = `${testPrefix}about`;
+      msg.attachments = new discordjs.Collection();
+
+      const clientInt = extendsClientToClientInt(client);
+      clientInt.prefix = { server_id: testPrefix };
+      clientInt.user = mock<discordjs.User>();
+      clientInt.commands = { about: mockCmd("about", aboutStub) };
+      clientInt.customListeners = await getListeners();
+      Object.keys(clientInt.customListeners).forEach((key) => {
+        const stub = sandbox.stub();
+        stub.resolves();
+        clientInt.customListeners[key] = mockListener(key, stub);
+      });
+
+      await onMessage(msg, clientInt);
+
+      expect(aboutStub).not.called;
+    });
+  });
+
+  context("command does not start with prefix", () => {
+    it("should return without calling command", async () => {
+      const aboutStub = sandbox.stub().resolves();
+      const client = mock<discordjs.Client>();
+      const msg = mock<discordjs.Message>();
+      const author = mock<discordjs.User>();
+      author.id = "1";
+      msg.author = author;
+      msg.content = `>about`;
+      msg.attachments = new discordjs.Collection();
+      msg.channel.startTyping = sandbox.stub();
+      msg.channel.stopTyping = sandbox.stub();
+      msg.channel.send = sandbox.stub().resolves();
+      msg.guild.id = "server_id";
+
+      const clientInt = extendsClientToClientInt(client);
+      clientInt.prefix = { server_id: testPrefix };
+      clientInt.commands = { about: mockCmd("about", aboutStub) };
+      clientInt.customListeners = await getListeners();
+      Object.keys(clientInt.customListeners).forEach((key) => {
+        const stub = sandbox.stub();
+        stub.resolves();
+        clientInt.customListeners[key] = mockListener(key, stub);
+      });
+
+      await onMessage(msg, clientInt);
+
+      expect(aboutStub).not.called;
     });
   });
 
@@ -120,7 +218,7 @@ describe("onMessage event", () => {
         });
 
         const msgPromise = onMessage(msg, clientInt);
-        await sandbox.clock.tickAsync(3000);
+        await sandbox.clock.tickAsync(MAGIC_TIMEOUT);
         await msgPromise;
 
         const inUselistener =
@@ -152,7 +250,7 @@ describe("onMessage event", () => {
       });
 
       const msgPromise = onMessage(msg, clientInt);
-      await sandbox.clock.tickAsync(3000);
+      await sandbox.clock.tickAsync(MAGIC_TIMEOUT);
 
       await msgPromise;
 
