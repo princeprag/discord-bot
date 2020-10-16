d2cd12e1d9a89237c1fa8158da8c18ec47ad0cd7
A. D. Prasad <adprasad@users.noreply.github.com>
2020-10-15 19:54:11 -0700

    test(command): add tests for listener command

--- src/commands/bot/listeners.ts
+++ src/commands/bot/listeners.ts
@@ -1,6 +1,12 @@
 import CommandInt from "@Interfaces/CommandInt";
 import { MessageEmbed } from "discord.js";
 
+const LISTENERS_CONSTANT = {
+  title: "I am always listening...",
+  description:
+    "For my commands to work, I have to listen to every message in the server. I check each message to see if you have called for my assistance. But did you know I also listen for other events? Here's what they are!",
+};
+
 const listeners: CommandInt = {
   names: ["listen", "listeners"],
   description:
@@ -15,12 +21,10 @@ const listeners: CommandInt = {
     listeners.setColor(bot.color);
 
     // Add the title.
-    listeners.setTitle("I am always listening...");
+    listeners.setTitle(LISTENERS_CONSTANT.title);
 
     // Add the description.
-    listeners.setDescription(
-      "For my commands to work, I have to listen to every message in the server. I check each message to see if you have called for my assistance. But did you know I also listen for other events? Here's what they are!"
-    );
+    listeners.setDescription(LISTENERS_CONSTANT.description);
 
     // Add the listeners to fields.
     for (const listener of Object.values(bot.customListeners)) {
