11b769603aeaf629f02e5249313c3f23ee74b0f5
nhcarrigan <nhcarrigan@gmail.com>
2020-10-16 11:33:04 -0700

    Modularise art for future additions, fix command

---
+++ src/utils/commands/artList.ts
@@ -0,0 +1,29 @@
+import { ArtInt } from "@Interfaces/commands/ArtInt";
+
+export const artList: ArtInt[] = [
+  {
+    file_name: "Moon.jpg",
+    artist: "Moonlight",
+    artist_url: "https://www.instagram.com/moonlightkcreations/",
+  },
+  {
+    file_name: "Moonlight 3.png",
+    artist: "Moonlight",
+    artist_url: "https://www.instagram.com/moonlightkcreations/",
+  },
+  {
+    file_name: "Moonlight 4.png",
+    artist: "Moonlight",
+    artist_url: "https://www.instagram.com/moonlightkcreations/",
+  },
+  {
+    file_name: "Moonlight 5.png",
+    artist: "Moonlight",
+    artist_url: "https://www.instagram.com/moonlightkcreations/",
+  },
+  {
+    file_name: "Moonlight.png",
+    artist: "Moonlight",
+    artist_url: "https://www.instagram.com/moonlightkcreations/",
+  },
+];
