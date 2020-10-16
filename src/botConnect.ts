631f0c2e85d7f5c8116fc26ac46d1e3bc35ba5c2
nhcarrigan <nhcarrigan@gmail.com>
2020-10-15 18:12:34 -0700

    Change DB message

--- src/botConnect.ts
+++ src/botConnect.ts
@@ -41,9 +41,6 @@ export async function botConnect(): Promise<void> {
     );
   }
 
-  // Connect to the MongoDB database.
-  await connectDatabase(debugChannelHook).then(loadCurrentTrackingOptOutList);
-
   // Create a new Discord bot object.
   const client: ClientInt = extendsClientToClientInt(new Client());
 
@@ -104,6 +101,11 @@ export async function botConnect(): Promise<void> {
 
   // Log the bot with the Discord token.
   await client.login(process.env.DISCORD_TOKEN);
+
+  // Connect to the MongoDB database.
+  await connectDatabase(debugChannelHook, client).then(
+    loadCurrentTrackingOptOutList
+  );
 
   // Send a debug log before turn off the bot.
   process.once("beforeExit", () => {
