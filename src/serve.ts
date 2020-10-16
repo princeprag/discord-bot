67460c1b7cd1a33547818d995feff76769e9b4df
nhcarrigan <nhcarrigan@gmail.com>
2020-10-16 09:25:29 -0700

    Revert

--- src/serve.ts
+++ src/serve.ts
@@ -1,10 +1,4 @@
 import { botConnect } from "./botConnect";
-import express from "express";
+
 
 botConnect().catch(console.log);
-
-const app = express();
-
-app.use("/", (_, res) => {
-  res.status(200).json({ message: "All good!" });
-});
