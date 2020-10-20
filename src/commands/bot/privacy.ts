abbd88996cc6d12ed523f21556b79f01d71f5202
A. D. Prasad <adprasad@users.noreply.github.com>
2020-10-19 20:01:52 -0700

    test(command): add tests for privacy command

--- src/commands/bot/privacy.ts
+++ src/commands/bot/privacy.ts
@@ -1,6 +1,12 @@
 import CommandInt from "@Interfaces/CommandInt";
 import { MessageEmbed } from "discord.js";
 
+const PRIVACY_CONSTANTS = {
+  title: "Privacy Policy",
+  description:
+    "As part of my features, I collect and use some specific Discord related information. This information includes, but may not be limited to, your user name, nickname, and Discord ID. If you do not want this information to be collected, please use my `optout` command. This will disable some cool features for your account, like my levelling system! [View my full policy](https://github.com/nhcarrigan/BeccaBot/blob/main/PRIVACY.md)",
+};
+
 const privacy: CommandInt = {
   name: "privacy",
   description:
@@ -11,10 +17,8 @@ const privacy: CommandInt = {
     // Send an embed message to the current channel.
     await channel.send(
       new MessageEmbed()
-        .setTitle("Privacy Policy")
-        .setDescription(
-          "As part of my features, I collect and use some specific Discord related information. This information includes, but may not be limited to, your user name, nickname, and Discord ID. If you do not want this information to be collected, please use my `optout` command. This will disable some cool features for your account, like my levelling system! [View my full policy](https://github.com/nhcarrigan/BeccaBot/blob/main/PRIVACY.md)"
-        )
+        .setTitle(PRIVACY_CONSTANTS.title)
+        .setDescription(PRIVACY_CONSTANTS.description)
     );
   },
 };
