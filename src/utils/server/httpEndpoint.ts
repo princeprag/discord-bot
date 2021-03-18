aeec0c446085f070cc7d9533a5ff4ba87bdf3ab4
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-17 19:50:55 -0700

    fix: server tweak

--- src/utils/server/httpEndpoint.ts
+++ src/utils/server/httpEndpoint.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http";
 import https from "https";
 
 export const endpoint = (): void => {
@@ -8,9 +9,18 @@ export const endpoint = (): void => {
     res.status(200).send("Ping!");
   });
 
-  const server = https.createServer(HTTPEndpoint);
+  const httpPort = process.env.PRODDEV === "production" ? 80 : 8080;
 
-  server.listen(8000, () => {
-    console.log("server is live on port 8000");
+  const httpServer = http.createServer(HTTPEndpoint);
+
+  httpServer.listen(httpPort, () => {
+    console.log(`http server is live on port ${httpPort}`);
   });
+
+  if (process.env.PRODDEV === "production") {
+    const httpsServer = https.createServer(HTTPEndpoint);
+    httpsServer.listen(443, () => {
+      console.log("https server is live on port 443");
+    });
+  }
 };
