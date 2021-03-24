be00207bb0b6d549cd31611ed77150480023f11b
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-23 21:44:11 -0700

    feat: initialise Sentry
    Initialise Sentry on bot load.

--- src/serve.ts
+++ src/serve.ts
@@ -1,3 +1,15 @@
 import { botConnect } from "./botConnect";
+import * as Sentry from "@sentry/node";
+import { RewriteFrames } from "@sentry/integrations";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  integrations: [
+    new RewriteFrames({
+      root: global.__dirname,
+    }),
+  ],
+});
 
 botConnect().catch(console.log);
