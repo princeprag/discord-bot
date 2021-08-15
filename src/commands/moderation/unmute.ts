d83def48e453e7fd34f782fb1d5af5878efa4740
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-08-15 09:40:26 -0700

    feat: migrate unmute command

--- src/commands/moderation/unmute.ts
+++ src/commands/moderation/unmute.ts
@@ -1,9 +1,7 @@
-import { MessageEmbed } from "discord.js";
 import { CommandInt } from "../../interfaces/commands/CommandInt";
 import { errorEmbedGenerator } from "../../modules/commands/errorEmbedGenerator";
-import { sendLogEmbed } from "../../modules/guild/sendLogEmbed";
+import { migrationEmbedGenerator } from "../../modules/commands/migrationEmbedGenerator";
 import { beccaErrorHandler } from "../../utils/beccaErrorHandler";
-import { customSubstring } from "../../utils/customSubstring";
 
 export const unmute: CommandInt = {
   name: "unmute",
@@ -12,93 +10,13 @@ export const unmute: CommandInt = {
     "`user`: @mention or ID of user to unmute",
     "`reason?`: reason for unmute",
   ],
+  isMigrated: true,
   category: "mod",
-  run: async (Becca, message, config) => {
+  run: async (Becca, message) => {
     try {
-      const { guild, member, content } = message;
-      const [, user, ...reason] = content.split(" ");
-
-      if (!guild) {
-        return {
-          success: false,
-          content: "I cannot seem to locate your guild record.",
-        };
-      }
-
-      if (!member || !member.permissions.has("KICK_MEMBERS")) {
-        return {
-          success: false,
-          content: "You do not have the correct skills to use this spell.",
-        };
-      }
-
-      const targetUser = user
-        ? await guild.members.fetch(`${BigInt(user.replace(/\D/g, ""))}`)
-        : null;
-      if (!targetUser) {
-        return {
-          success: false,
-          content: "Who did you want me to remove a curse from?",
-        };
-      }
-
-      const mutedRole = guild.roles.cache.find(
-        (el) => el.id === config.muted_role
-      );
-
-      if (!mutedRole) {
-        return {
-          success: false,
-          content: "I do not know the magic words to lift the curse.",
-        };
-      }
-
-      if (targetUser.id === member.id) {
-        return {
-          success: false,
-          content:
-            "The fact that you are asking this means you aren't silenced...",
-        };
-      }
-
-      if (targetUser.id === Becca.user?.id) {
-        return {
-          success: false,
-          content: "Bold of you to assume anyone could silence me.",
-        };
-      }
-
-      if (!targetUser.roles.cache.has(mutedRole.id)) {
-        return {
-          success: false,
-          content: "That user isn't cursed at this time.",
-        };
-      }
-
-      await targetUser.roles.remove(mutedRole);
-
-      const muteEmbed = new MessageEmbed();
-      muteEmbed.setTitle("A user is no longer silenced!");
-      muteEmbed.setDescription(
-        `The curse was lifted by ${member.user.username}`
-      );
-      muteEmbed.setColor(Becca.colours.success);
-      muteEmbed.addField(
-        "Reason",
-        customSubstring(reason.join(" ") || "They did not tell me why.", 1000)
-      );
-      muteEmbed.setFooter(`ID: ${targetUser.id}`);
-      muteEmbed.setTimestamp();
-      muteEmbed.setAuthor(
-        `${targetUser.user.username}#${targetUser.user.discriminator}`,
-        targetUser.user.displayAvatarURL()
-      );
-
-      await sendLogEmbed(Becca, guild, muteEmbed);
-
       return {
-        success: true,
-        content: "The curse has been lifted from that user.",
+        success: false,
+        content: migrationEmbedGenerator("mod unmute"),
       };
     } catch (err) {
       const errorId = await beccaErrorHandler(
--- src/slash/moderation/mod.ts
+++ src/slash/moderation/mod.ts
@@ -5,6 +5,7 @@ import {
 import { SlashInt } from "../../interfaces/slash/SlashInt";
 import { errorEmbedGenerator } from "../../modules/commands/errorEmbedGenerator";
 import { handleMute } from "../../modules/slash/moderation/handleMute";
+import { handleUnmute } from "../../modules/slash/moderation/handleUnmute";
 import { handleWarn } from "../../modules/slash/moderation/handleWarn";
 import { handleWarnCount } from "../../modules/slash/moderation/handleWarnCount";
 import { beccaErrorHandler } from "../../utils/beccaErrorHandler";
@@ -47,13 +48,29 @@ export const mod: SlashInt = {
         .addUserOption((option) =>
           option
             .setName("target")
-            .setDescription("The user to warn.")
+            .setDescription("The user to mute.")
             .setRequired(true)
         )
         .addStringOption((option) =>
           option
             .setName("reason")
-            .setDescription("The reason for issuing this warning.")
+            .setDescription("The reason for muting the user.")
+        )
+    )
+    .addSubcommand(
+      new SlashCommandSubcommandBuilder()
+        .setName("unmute")
+        .setDescription("Unmutes a user via your configured muted role.")
+        .addUserOption((option) =>
+          option
+            .setName("target")
+            .setDescription("The user to unmute.")
+            .setRequired(true)
+        )
+        .addStringOption((option) =>
+          option
+            .setName("reason")
+            .setDescription("The reason for unmuting the user.")
         )
     ),
   run: async (Becca, interaction, config) => {
@@ -70,6 +87,9 @@ export const mod: SlashInt = {
           break;
         case "mute":
           await handleMute(Becca, interaction, config);
+          break;
+        case "unmute":
+          await handleUnmute(Becca, interaction, config);
           break;
         default:
           await interaction.editReply(Becca.responses.invalid_command);
