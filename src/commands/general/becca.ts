b1cf52a9c5d082b5c97d08f638255dc4714ec960
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-08-17 17:48:34 -0700

    feat: migrate becca command

--- src/commands/general/becca.ts
+++ src/commands/general/becca.ts
@@ -1,6 +1,6 @@
-import { MessageEmbed } from "discord.js";
 import { CommandInt } from "../../interfaces/commands/CommandInt";
 import { errorEmbedGenerator } from "../../modules/commands/errorEmbedGenerator";
+import { migrationEmbedGenerator } from "../../modules/commands/migrationEmbedGenerator";
 import { beccaErrorHandler } from "../../utils/beccaErrorHandler";
 
 export const becca: CommandInt = {
@@ -8,16 +8,10 @@ export const becca: CommandInt = {
   description: "Returns information about Becca's character.",
   category: "general",
   parameters: [],
+  isMigrated: true,
   run: async (Becca, message) => {
     try {
-      const beccaEmbed = new MessageEmbed();
-      beccaEmbed.setColor(Becca.colours.default);
-      beccaEmbed.setTitle("Becca Lyria");
-      beccaEmbed.setDescription(
-        "If you want to read about my adventures, check my [profile site](https://www.beccalyria.com). I would rather not have to recount them all here."
-      );
-      beccaEmbed.setThumbnail(Becca.user?.avatarURL({ dynamic: true }) || "");
-      return { success: true, content: beccaEmbed };
+      return { success: true, content: migrationEmbedGenerator("becca becca") };
     } catch (err) {
       const errorId = await beccaErrorHandler(
         Becca,
