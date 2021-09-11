794c81d4298f407c254d327483d51abfe83f45a9
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-11 09:43:44 -0700

    feat: add update command

---
+++ src/config/commands/updatesData.ts
@@ -0,0 +1,5 @@
+export const updatesSinceLastRelease: string[] = [
+  "- Added `updates` command to show latest changes and explain new release schedule.",
+];
+
+export const nextScheduledRelease = "<t:1632157200:F>";
---
+++ src/modules/commands/subcommands/becca/handleUpdates.ts
@@ -0,0 +1,50 @@
+/* eslint-disable jsdoc/require-param */
+import { MessageEmbed } from "discord.js";
+
+import {
+  nextScheduledRelease,
+  updatesSinceLastRelease,
+} from "../../../../config/commands/updatesData";
+import { CommandHandler } from "../../../../interfaces/commands/CommandHandler";
+import { beccaErrorHandler } from "../../../../utils/beccaErrorHandler";
+import { errorEmbedGenerator } from "../../errorEmbedGenerator";
+
+/**
+ * Generates an embed explaining the new release schedule, and what the update
+ * process breaks in terms of lost cache.
+ */
+export const handleUpdates: CommandHandler = async (Becca, interaction) => {
+  try {
+    const updateEmbed = new MessageEmbed();
+    updateEmbed.setTitle("Update Information");
+    updateEmbed.setDescription(
+      "Becca's updates are deployed every Monday at 10AM Pacific Time. This is important information to know, as these deployments clear the cache. This results in any outstanding cache-reliant features, such as polls, trivia games, or scheduled posts, to be lost. Please plan your interactions around this schedule."
+    );
+    updateEmbed.addField("Latest Updates", updatesSinceLastRelease.join("\n"));
+    updateEmbed.addField("Next Scheduled Update", nextScheduledRelease);
+    updateEmbed.addField(
+      "Changelog",
+      "View Becca's entire change log [in her documentation](https://docs.beccalyria.com/#/changelog)."
+    );
+    updateEmbed.setColor(Becca.colours.default);
+    await interaction.editReply({ embeds: [updateEmbed] });
+  } catch (err) {
+    const errorId = await beccaErrorHandler(
+      Becca,
+      "ping command",
+      err,
+      interaction.guild?.name
+    );
+    await interaction
+      .reply({
+        embeds: [errorEmbedGenerator(Becca, "ping", errorId)],
+        ephemeral: true,
+      })
+      .catch(
+        async () =>
+          await interaction.editReply({
+            embeds: [errorEmbedGenerator(Becca, "ping", errorId)],
+          })
+      );
+  }
+};
