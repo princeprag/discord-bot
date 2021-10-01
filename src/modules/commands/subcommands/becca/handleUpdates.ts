bd57587bdbea629df622c942473508be66595701
Prince <princepragsah4375@gmail.com>
2021-10-01 09:24:59 +0530

    version field added

--- src/modules/commands/subcommands/becca/handleUpdates.ts
+++ src/modules/commands/subcommands/becca/handleUpdates.ts
@@ -21,6 +21,10 @@ export const handleUpdates: CommandHandler = async (Becca, interaction) => {
       "Becca's updates are deployed every Monday at 10AM Pacific Time. This is important information to know, as these deployments clear the cache. This results in any outstanding cache-reliant features, such as polls, trivia games, or scheduled posts, to be lost. Please plan your interactions around this schedule."
     );
     updateEmbed.addField("Latest Updates", updatesSinceLastRelease.join("\n"));
+    updateEmbed.addField(
+        "Current Version",
+        process.env.npm_package_version || "0.0.0"
+    );
     updateEmbed.addField("Next Scheduled Update", nextScheduledRelease);
     updateEmbed.addField(
       "Changelog",
